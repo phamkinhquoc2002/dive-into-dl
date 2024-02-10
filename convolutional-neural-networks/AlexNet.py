10a730c8633366c4a2e49fff08d65d058db4b938
kingquoc <phamkinhquoc2002@gmail.com>
2024-02-10 23:07:24 +0700

    Googlenet

--- convolutional-neural-networks/AlexNet.py
+++ convolutional-neural-networks/AlexNet.py
@@ -6,26 +6,25 @@ from config import init_cnn
 
 
 class AlexNet(d2l.Classifier):
-    def __init__(self, lr = 0.1, num_classes = 10):
+    def __init__(self, lr=0.1, num_classes=10):
         super().__init__()
         self.save_hyperparameters()
         self.net = nn.Sequential(
-            nn.LazyConv2d(96, kernel_size=4, stride=4, padding=1), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2), 
+            nn.LazyConv2d(96, kernel_size=11, stride=4, padding=1),
+            nn.ReLU(), nn.MaxPool2d(kernel_size=3, stride=2),
             nn.LazyConv2d(256, kernel_size=5, padding=2), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2), 
+            nn.MaxPool2d(kernel_size=3, stride=2),
             nn.LazyConv2d(384, kernel_size=3, padding=1), nn.ReLU(),
             nn.LazyConv2d(384, kernel_size=3, padding=1), nn.ReLU(),
             nn.LazyConv2d(256, kernel_size=3, padding=1), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride = 2), nn.Flatten(),
-            nn.LazyLinear(4096),
-            nn.LazyLinear(4096),
-            nn.LazyLinear(num_classes)
-        )
+            nn.MaxPool2d(kernel_size=3, stride=2), nn.Flatten(),
+            nn.LazyLinear(4096), nn.ReLU(), nn.Dropout(p=0.5),
+            nn.LazyLinear(4096), nn.ReLU(),nn.Dropout(p=0.5),
+            nn.LazyLinear(num_classes))
         self.net.apply(init_cnn)
 
 if __name__ == '__main__':
     model = AlexNet(lr=0.1)
-    data = d2l.FashionMNIST(batch_size=128, resize=(32, 32))
+    data = d2l.FashionMNIST(batch_size=128, resize=(224, 224))
     trainer = d2l.Trainer(max_epochs=10, num_gpus=1)
     trainer.fit(model, data)
--- convolutional-neural-networks/GoogLENET.py
+++ convolutional-neural-networks/GoogLENET.py
@@ -25,3 +25,44 @@ class Inception(nn.Module):
         b3 = F.relu(self.b3_2(self.b3_1(x)))
         b4 = F.relu(self.b4_2(self.b4_1))
         return torch.concat((b1, b2, b3, b4), dim=1)
+    
+class GoogLENET(d2l.Classifier):
+    def b1(self):
+        return nn.Sequential(
+            nn.LazyConv2d(64, kernel_size=7, stride=2, padding=3),
+            nn.ReLU(), nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+    def b2(self):
+        return nn.Sequential(
+            nn.LazyConv2d(64, kernel_size=1), nn.ReLU(),
+            nn.LazyConv2d(64, kernel_size=1), nn.ReLU(),
+            nn.LazyConv2d(64, kernel_size=1), nn.ReLU(),
+        )
+    def b3(self):
+        return nn.Sequential(Inception(64, (96, 128), (16, 32), 32),
+                             Inception(128, (128, 192), (32, 96), 64),
+                             nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    def b4(self):
+        return nn.Sequential(Inception(192, (96, 208), (16, 48), 64), 
+                             Inception(160, (112, 224), (24, 64), 64),
+                             Inception(128, (128, 256), (24, 64), 64),
+                             Inception(112, (144, 288), (32, 64), 64),
+                             Inception(256, (160, 320), (32, 128), 128),
+                             nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    def b5(self):
+        return nn.Sequential(Inception(256, (160, 320), (32, 128), 128),
+                             Inception(384, (192, 384), (48, 128), 128),
+                             nn.AdaptiveAvgPool2d((1,1)), nn.Flatten())
+    def __init__(self, lr=0.1, num_classes=10):
+        super(GoogLENET, self).__init__()
+        self.save_hyperparameters()
+        self.net = nn.Sequential(self.b1(), self.b2(), self.b3(), self.b4(),
+        self.b5(), nn.LazyLinear(num_classes))
+        self.net.apply(d2l.init_cnn)
+
+if __name__ == '__main__':
+    model = GoogleNet(lr=0.01)
+    trainer = d2l.Trainer(max_epochs=10, num_gpus=1)
+    data = d2l.FashionMNIST(batch_size=128, resize=(96, 96))
+    model.apply_init([next(iter(data.get_dataloader(True)))[0]], d2l.init_cnn)
+    trainer.fit(model, data)
